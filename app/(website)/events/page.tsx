2e9d00b0d8f7a84ce144b91b39ba7b38d92dc73f
Weslley Botelho <weslley.santtos01@gmail.com>
2024-11-19 11:52:16 +0100

    annucements nov and dec

--- app/(website)/events/page.tsx
+++ app/(website)/events/page.tsx
@@ -5,23 +5,53 @@ import { useGSAP } from '@gsap/react';
 
 const events2 = [
   {
-    month: 'OCT',
+    month: 'NOV',
+    day: 19,
+    title: 'Prayer Meeting',
+    time: '07:30 PM',
+  },
+  {
+    month: 'NOV',
+    day: 22,
+    title: 'Evangelism - Gare Central',
+    time: '06:30 PM',
+  },
+  {
+    month: 'NOV',
+    day: 24,
+    title: "Women's Meeting - Anne Christine",
+    time: 'After service',
+  },
+  {
+    month: 'NOV',
+    day: 26,
+    title: "Men's Ministry - Boxing Activity",
+    time: '7:30 PM',
+    // location: 'Gare Central - Main entrance',
+  },
+  {
+    month: 'NOV',
+    day: 29,
+    title: 'Youth Meeting - Mélodie',
+    time: '7:00 PM',
+  },
+  {
+    month: 'DEC',
     day: 15,
-    title: "Men's ministry",
-    time: '7:30 AM - 8:30 AM',
+    title: 'Baptism',
+    time: 'For more info - Lionel or Laurianne',
   },
   {
-    month: 'OCT',
-    day: 20,
-    title: 'Youth meeting',
-    time: '7:30 AM - 8:30 AM',
+    month: 'DEC',
+    day: 22,
+    title: 'Christimas - Potluck (Bring your traditional dish)',
+    time: 'After service',
   },
   {
-    month: 'OCT',
-    day: 2,
-    title: 'Evangelism',
-    time: '7:30 AM - 8:30 AM',
-    location: 'Gare Central - Main entrance',
+    month: 'DEC',
+    day: 31,
+    title: 'New Years Eve - 25€ per person',
+    time: 'Ticket non-refundable (Free for kids -12 yrs old)',
   },
 ];
 
@@ -35,7 +65,7 @@ export default function Events() {
         <section className="container mt-3">
           <div className={styles.container}>
             <h1 className={styles.heading}>Calendar</h1>
-            <h2 className={styles.subheading + ' mt-5'}>October 2024</h2>
+            <h2 className={styles.subheading + ' mt-5'}>2024</h2>
             <div id="events">
               {events2.map((event, index) => (
                 <div key={index} className={`${styles.event} flex-start-row pt-2`}>
@@ -46,7 +76,7 @@ export default function Events() {
                   <div>
                     <p className={styles.eventTitle}>{event.title}</p>
                     {event.time && <p className={styles.eventTime}>{event.time}</p>}
-                    <p className={styles.eventLocation}>{event.location}</p>
+                    {/* <p className={styles.eventLocation}>{event.location}</p> */}
                   </div>
                 </div>
               ))}
